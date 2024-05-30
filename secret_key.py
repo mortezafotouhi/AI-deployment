9916124acb9812164e2b090abda7074d3c792a2f
Morteza Fotouhi <138924204+mortezafotouhi@users.noreply.github.com>
2024-05-30 17:57:04 +0100

    Delete secret_key.py

--- secret_key.py
+++ secret_key.py
@@ -1,4 +1,4 @@
-key = "38e8d973f903407782913bbc8c64209d"
-key_us = "4279de769f734565875ea131fbae606f"
+key = ""
+key_us = ""
 endpoint = "https://huma-testing.openai.azure.com/"
 endpoint_us = "https://openai-us-prod.openai.azure.com"
